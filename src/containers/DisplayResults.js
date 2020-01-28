9fe268b6b7a440a39dc11c14bb5bad86ffb6923b
nevtay <nevintay@gmail.com>
2020-01-28 11:04:09 +0800

    minor copy change

--- src/containers/DisplayResults.js
+++ src/containers/DisplayResults.js
@@ -13,15 +13,18 @@ class DisplayResults extends React.Component {
         return(
             <section className="tdee-results">
                 
-                <h2 className="tdee-results__title">Based on the input provided, your measurements are as follows: </h2>
+                <h2 className="tdee-results__title">Your measurements are as follows: </h2>
+                
                 <div className="tdee-resultsSubcontainer">
-                    <h3 className="tdee-resultsSubcontainer__title">TDEE:</h3>
+                    <h3 className="tdee-resultsSubcontainer__title">TDEE: {this.props.val}</h3>
                 </div>
+
                 <div className="tdee-resultsSubcontainer">
-                    <h3 className="tdee-resultsSubcontainer__title">BMI:</h3>
+                    <h3 className="tdee-resultsSubcontainer__title">BMI: {this.props.val}</h3>
                 </div>
+                
                 <div className="tdee-resultsSubcontainer">
-                    <h3 className="tdee-resultsSubcontainer__title">BMR:</h3>
+                    <h3 className="tdee-resultsSubcontainer__title">BMR: {this.props.val}</h3>
                 </div>
 
             </section>
