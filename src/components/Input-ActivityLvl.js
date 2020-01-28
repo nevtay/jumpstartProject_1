a51ab4c28055f6774b9da5451cb01eaaecd04109
nevtay <nevintay@gmail.com>
2020-01-28 14:30:30 +0800

    removed unncessary setState function; changed 'Very High' value from 'very-high' to 'very high'; changed default option copy

--- src/components/Input-ActivityLvl.js
+++ src/components/Input-ActivityLvl.js
@@ -11,9 +11,6 @@ class InputActivityLvl extends React.Component {
 
     updateActivityLevel = (e) => {
         this.props.activityLevelUpdate(e.target.value);
-        this.setState({
-            activityLevel: e.target.value,
-        })
     }
 
 render() {
@@ -22,11 +19,11 @@ render() {
         <fieldset id="activity-level">
             <label for="activity-level">Activity Level</label>
                 <select className="tdee-inputs__label tdee-input__activityLevel" onChange={this.updateActivityLevel}>
-                    <option value="none" selected disabled hidden>Choose</option>
+                    <option value="none" selected disabled hidden>Choose...</option>
                     <option value="low">Low</option>
                     <option value="moderate">Moderate</option>
                     <option value="high">High</option>
-                    <option value="very-high">Very High</option>
+                    <option value="very high">Very High</option>
                 </select>
         </fieldset>
         )
