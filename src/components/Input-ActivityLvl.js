e3c4dade97949634fc9c1f03d0bb7db83c26a945
nevtay <nevintay@gmail.com>
2020-01-28 17:39:20 +0800

    minor edits

--- src/components/Input-ActivityLvl.js
+++ src/components/Input-ActivityLvl.js
@@ -17,14 +17,24 @@ render() {
 
     return(
         <fieldset id="activity-level">
-            <label for="activity-level">Activity Level</label>
-                <select className="tdee-inputs__label tdee-input__activityLevel" onChange={this.updateActivityLevel}>
+            
+            <label 
+            for="activity-level">
+                Activity Level
+                </label>
+
+                <select 
+                className="tdee-inputs__label tdee-input__activityLevel" 
+                onChange={this.updateActivityLevel}>
+                    
                     <option value="none" selected disabled hidden>Choose...</option>
                     <option value="low">Low</option>
                     <option value="moderate">Moderate</option>
                     <option value="high">High</option>
                     <option value="very high">Very High</option>
+                    
                 </select>
+
         </fieldset>
         )
     }
