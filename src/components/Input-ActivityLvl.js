5eb3dd04de13c1306d75062246bff52db676f375
nevtay <nevintay@gmail.com>
2020-01-28 11:36:27 +0800

    added props to function; added unselectable default option to select field

--- src/components/Input-ActivityLvl.js
+++ src/components/Input-ActivityLvl.js
@@ -6,17 +6,13 @@ class InputActivityLvl extends React.Component {
 
         this.state = {
             activityLevel: '',
-            label: '',
         }
     }
 
     updateActivityLevel = (e) => {
-        let index = e.target.selectedIndex;
-        let label = e.target[index].text;
-        let value = e.target.value;
+        this.props.activityLevelUpdate(e.target.value);
         this.setState({
-            activityLevel: value,
-            label: label,
+            activityLevel: e.target.value,
         })
     }
 
@@ -25,8 +21,8 @@ render() {
     return(
         <fieldset id="activity-level">
             <label for="activity-level">Activity Level</label>
-                <select className="tdee-inputs__label tdee-input__activityLevel" value={this.state.activityLevel} onChange={this.updateActivityLevel}>
-                    <option value="select" disabled>Select</option>
+                <select className="tdee-inputs__label tdee-input__activityLevel" onChange={this.updateActivityLevel}>
+                    <option value="none" selected disabled hidden>Choose</option>
                     <option value="low">Low</option>
                     <option value="moderate">Moderate</option>
                     <option value="high">High</option>
