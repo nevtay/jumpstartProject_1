8260478770c81be80ae8b982de8cb949aeed6047
nevtay <nevintay@gmail.com>
2020-01-28 14:32:11 +0800

    removed round brackets from setState arguments; added setState for activityLevel

--- src/containers/InputsForCalculator.js
+++ src/containers/InputsForCalculator.js
@@ -4,6 +4,7 @@ import InputAge from "../components/Input-Age"
 import InputHeight from "../components/Input-Height"
 import InputWeight from "../components/Input-Weight"
 import InputActivityLevel from "../components/Input-ActivityLvl"
+import "./InputsForCalculator.css"
 
 class CalcInputs extends React.Component {
     constructor(props) {
@@ -18,52 +19,95 @@ class CalcInputs extends React.Component {
         }
     }
 
-    genderUpdate = (gender) => {
+    genderUpdate = gender => {
         this.setState({
             gender: gender,
         })
     }
 
-    ageUpdate = (age) => {
+    ageUpdate = age => {
         this.setState({
             age: age,
         })
     }
 
-    heightUpdate = (height) => {
+    heightUpdate = height => {
         this.setState({
             height: height,
         })
     }
 
-    weightUpdate = (weight) => {
+    weightUpdate = weight => {
         this.setState({
             weight: weight,
         })
     }
 
+    activityLevelUpdate = activityLevel => {
+        this.setState({
+            activityLevel: activityLevel,
+        })
+    }
+
     render() {
+        
+    const calculateBMR = () => {
+        let { gender, age, height, weight, activityLevel } = this.state;
+        console.log(gender, age, height, weight, activityLevel);
+    }
+
+    const ageLimit = age => {
+        age = Number(age);
+        const maxAge = 130;
+        if (age === 0) {
+            return ` 0 years old.`;
+        }
+        if (age < 0) {
+            return ` Error: ${this.state.age} is an invalid age.`
+        }
+        if (age > maxAge) {
+            return ``
+        }
+        return Math.round(age);
+    }
         return (
-            <section className="tdee-inputs">
-                <form onChange={console.log(this.state)}>
+            
+            <section className="tdee-container">
+                <form className="tdee-inputs">
 
-                <InputGender genderUpdate={this.genderUpdate} />
-                <h1>Gender is {this.state.gender}</h1>
+                    <InputGender genderUpdate={this.genderUpdate} />
 
-                <InputAge ageUpdate={this.ageUpdate} />
-                <h1>Age is {this.state.age} years old</h1>
+                    <InputAge ageUpdate={this.ageUpdate} />
+                        <span className={this.state.age > 130 ? "revealed" : "hidden"}>Age </span>
 
-                <InputHeight heightUpdate={this.heightUpdate} />
-                <h1>Height is {this.state.height}cm</h1>
+                    <InputHeight heightUpdate={this.heightUpdate} />
 
-                <InputWeight weightUpdate={this.weightUpdate} />
-                <h1>Weight is {this.state.weight}kg</h1>
+                    <InputWeight weightUpdate={this.weightUpdate} />
 
-                <InputActivityLevel />
-                <h1>Activity level is {this.state.weight}</h1>
+                    <InputActivityLevel activityLevelUpdate={this.activityLevelUpdate} />
 
                 </form>
 
+            <section className="tdee-results">
+                <h1>
+                    Gender is 
+                    { this.state.gender === '' ? " UNKNOWN " : ` ${this.state.gender} `}
+                    </h1>
+                    <h1>
+                        { ageLimit(this.state.age) } 
+                    </h1>
+                    <h1>
+                        Height is 
+                        { this.state.height === '' ? ` 0 ` : ` ${this.state.height} ` }cm
+                        </h1>
+                    <h1>
+                        Weight is 
+                        { this.state.weight === '' ? ` 0 ` : ` ${this.state.weight} ` }kg
+                        </h1>
+                    <h1>Activity level is { this.state.activityLevel === '' ? " UNKNOWN " : ` ${this.state.activityLevel.toUpperCase()} ` }</h1>
+            </section>
+                {calculateBMR()};
+            
             </section>
         )
     }
