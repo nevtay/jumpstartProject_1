d62e3dbbe798611184d4a4119701cb70a20c9662
nevtay <nevintay@gmail.com>
2020-01-29 10:36:53 +0800

    removed unused import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header"
 import CalcInputs from "./containers/InputsForCalculator"
-import DisplayResults from "./containers/DisplayResults"
 
 function App() {
   return (   
