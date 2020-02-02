7d91021a75199bcced9d21fc43c212fad471bf97
nevtay <nevintay@gmail.com>
2020-02-03 01:05:45 +0800

    added simple tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
+import DisplayInputs from './containers/DisplayInputs'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('Header component with data-testid="header" should load', () => {
+  const { getByTestId } = render(<App />)
+  const headerComponent = getByTestId('header')
+  expect(headerComponent).toBeInTheDocument()
+})
+
+test('DisplayInputs component with data-testid="DisplayInputs" should load', () => {
+  const { getByTestId } = render(<App />)
+  const DisplayInputsComponent = getByTestId('DisplayInputs')
+  expect(DisplayInputsComponent).toBeInTheDocument()
+})
+
+test('DisplayResults component with data-testid="DisplayResults" should load', () => {
+  const { getByTestId } = render(<DisplayInputs />)
+  const DisplayResultsComponent = getByTestId('DisplayResults')
+  expect(DisplayResultsComponent).toBeInTheDocument()
+})
