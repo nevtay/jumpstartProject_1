017fef3973b49cc220d6caf5492c56a756abf20e
nevtay <nevintay@gmail.com>
2020-04-15 16:55:06 +0800

    Abstract out gender setter function to parent component

--- src/components/Input-Gender.js
+++ src/components/Input-Gender.js
@@ -1,36 +1,35 @@
-import React from "react"
+import React from "react";
 
 class InputGender extends React.Component {
-
-    genderOfUser = (e) => {
-        this.props.setGender(e.target.value);
-    }
-
-render() {
-    return(
-        <fieldset id="gender">
-
+  render() {
+    return (
+      <fieldset id="gender">
         <label>Select Biological Gender: </label>
-            
-            <input 
-            type="radio" 
-            name="gender" 
-            id="male"
-            onChange={this.genderOfUser} 
-            value="male" /> 
-                <label className="tdee-inputs__label" htmlFor="male">Male</label>
-            
-            <input 
-            type="radio" 
-            name="gender" 
-            id="female" 
-            onChange={this.genderOfUser} 
-            value="female" />
-                <label className="tdee-inputs__label"  htmlFor="female">Female</label>
 
-        </fieldset>
-        )
-    }
+        <input
+          type="radio"
+          name="gender"
+          id="male"
+          onChange={this.props.setGender}
+          value="male"
+        />
+        <label className="tdee-inputs__label" htmlFor="male">
+          Male
+        </label>
+
+        <input
+          type="radio"
+          name="gender"
+          id="female"
+          onChange={this.props.setGender}
+          value="female"
+        />
+        <label className="tdee-inputs__label" htmlFor="female">
+          Female
+        </label>
+      </fieldset>
+    );
+  }
 }
 
 export default InputGender;
