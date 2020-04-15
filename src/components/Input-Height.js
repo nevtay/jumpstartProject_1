9141149cacc03fe47ff9f0ebc271582e79dda5ef
nevtay <nevintay@gmail.com>
2020-04-15 17:29:31 +0800

    Refactor code spacing for tidiness

--- src/components/Input-Height.js
+++ src/components/Input-Height.js
@@ -1,87 +1,107 @@
-import React from "react"
+import React from "react";
 
 class InputHeight extends React.Component {
-    inputsToIgnoreCm = (e) => {
-        if (e.charCode === 46 || e.charCode === 45 || e.charCode === 101 || e.target.value === "0" || e.target.value.length >= 3) {
-            e.preventDefault();
-        }
-    } 
-
-    inputsToIgnoreFeet = (e) => {
-        if (e.charCode === 46 || e.charCode === 45 || e.charCode === 101 || e.target.value === "0" || e.target.value.length >= 1) {
-            e.preventDefault();
-        } 
+  inputsToIgnoreCm = (e) => {
+    if (
+      e.charCode === 46 ||
+      e.charCode === 45 ||
+      e.charCode === 101 ||
+      e.target.value === "0" ||
+      e.target.value.length >= 3
+    ) {
+      e.preventDefault();
     }
+  };
 
-
-    inputsToIgnoreInches = (e) => {
-        if (e.charCode === 45 || e.charCode === 101 || e.target.value === "0" || e.target.value.length > 2 || e.target.value > 11) {
-            e.preventDefault();
-            return;
-        }
+  inputsToIgnoreFeet = (e) => {
+    if (
+      e.charCode === 46 ||
+      e.charCode === 45 ||
+      e.charCode === 101 ||
+      e.target.value === "0" ||
+      e.target.value.length >= 1
+    ) {
+      e.preventDefault();
     }
+  };
 
-render() {
+  inputsToIgnoreInches = (e) => {
+    if (
+      e.charCode === 45 ||
+      e.charCode === 101 ||
+      e.target.value === "0" ||
+      e.target.value.length > 2 ||
+      e.target.value > 11
+    ) {
+      e.preventDefault();
+      return;
+    }
+  };
+
+  render() {
     const measuringUnitToUse = this.props.setMeasuringUnit;
     let showCentimetres;
     let showFeet;
     let showInches;
 
     if (measuringUnitToUse === "metric") {
-        
-        showFeet = '';
+      showFeet = "";
 
-        showInches = '';
+      showInches = "";
 
-        showCentimetres = <input 
-        id="inputs-height" 
-        type="number"
-        value={this.props.heightInCm}
-        onChange={this.props.setHeightInCm}
-        onKeyPress={this.inputsToIgnoreCm}
-        placeholder="cm"
-         /> 
-
-    } 
-
-    if (measuringUnitToUse === "imperial") {
-        showCentimetres = '';
-
-        showFeet = <input 
-        type="number" 
-        value={this.props.heightInFeet} 
-        onChange={this.props.setHeightInFeet}
-        onKeyPress={this.inputsToIgnoreFeet}
-        onBlur={this.inputsToIgnoreFeet}
-        placeholder="ft"
-        />;
-        
-        showInches = <input 
-        type="number" 
-        value={this.props.heightInInches} 
-        onChange={this.props.setHeightInInches}
-        onKeyPress={this.inputsToIgnoreInches}
-        onBlur={this.inchesInvalidValuesCheck}
-        placeholder="inches" 
+      showCentimetres = (
+        <input
+          id="inputs-height"
+          type="number"
+          value={this.props.heightInCm}
+          onChange={this.props.setHeightInCm}
+          onKeyPress={this.inputsToIgnoreCm}
+          placeholder="cm"
         />
+      );
     }
 
-    return(
-        <fieldset id="height">
-           
-            <label 
-            htmlFor="height" 
-            className="tdee-inputs__label"> 
-            {this.props.setMeasuringUnit === "metric" ? "Height (cm)" : "Height (feet and inches)"}&nbsp;
-            </label>
+    if (measuringUnitToUse === "imperial") {
+      showCentimetres = "";
 
-                {showCentimetres}
-                {showFeet}
-                {showInches}
-                
-            </fieldset>
-            )
-    } 
+      showFeet = (
+        <input
+          type="number"
+          value={this.props.heightInFeet}
+          onChange={this.props.setHeightInFeet}
+          onKeyPress={this.inputsToIgnoreFeet}
+          onBlur={this.inputsToIgnoreFeet}
+          placeholder="ft"
+        />
+      );
+
+      showInches = (
+        <input
+          type="number"
+          value={this.props.heightInInches}
+          onChange={this.props.setHeightInInches}
+          onKeyPress={this.inputsToIgnoreInches}
+          onBlur={this.inchesInvalidValuesCheck}
+          placeholder="inches"
+        />
+      );
+    }
+
+    return (
+      <fieldset id="height">
+        <label htmlFor="height">
+          {this.props.setMeasuringUnit === "metric"
+            ? "Height (cm)"
+            : "Height (feet and inches)"}
+          &nbsp;
+        </label>
+
+        {showCentimetres}
+        {showFeet}
+        {showInches}
+      </fieldset>
+    );
+  }
 }
 
 export default InputHeight;
