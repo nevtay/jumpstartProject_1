558ea1c828e6fbf5c02d59960d871a55e3a61e38
nevtay <nevintay@gmail.com>
2020-04-17 02:56:50 +0800

    Refactor code; add conditional logic to ensure that BMR will return 0 unless all relevant fields are filled

--- src/containers/DisplayResults.js
+++ src/containers/DisplayResults.js
@@ -2,9 +2,8 @@ import React from 'react'
 import convert from 'convert-units'
 
 class DisplayResults extends React.Component {
-  render () {
-    // can we convert feet and inches to inches first? then separate it out into two input boxes
-    const {
+  render() {
+    let {
       gender,
       age,
       heightInCm,
@@ -16,19 +15,18 @@ class DisplayResults extends React.Component {
       unitType
     } = this.props.personalInfo
 
-    const calculateTDEE = ({ activityLevel } = this.props.personalInfo) => {
-      return Math.floor(calculateBMR() * activityLevel)
-    }
-
-    const calculateBMI = ({ heightInCm, heightInFeet, heightInInches, weightInKg, weightInLbs, unitType } = this.props.personalInfo) => {
+    const calculateBMI = () => {
+      const displayResult = (bmiValue) => {
+        return isNaN(bmiValue) || !isFinite(bmiValue) ? 0 : bmiValue.toFixed(2)
+      }
       if (unitType === 'metric') {
-        if (heightInCm === '' || weightInKg === '') {
+        if (!heightInCm || !weightInKg) {
           return 0
         }
         const weightFactor = Number(weightInKg)
         const heightInMetres = Number(heightInCm) / 100
-        const result = weightFactor / (Math.pow(heightInMetres, 2))
-        return result.toFixed(2)
+        const bmiMetric = weightFactor / Math.pow(heightInMetres, 2)
+        return displayResult(bmiMetric)
       } else {
         if (unitType === 'imperial') {
           if (!heightInInches && !heightInFeet) {
@@ -43,25 +41,40 @@ class DisplayResults extends React.Component {
           if (heightInFeet === '') {
             heightInFeet = 0
           }
-          console.log(weightInLbs, heightInFeet, heightInInches)
           const weightFactor = Number(weightInLbs)
           const feetToInches = convert(heightInFeet).from('ft').to('in')
-          const totalHeightInInches = Number(heightInInches) + Number(feetToInches)
-          const result = (weightFactor / (Math.pow(totalHeightInInches, 2))) * 703
-          return result.toFixed(2)
+          const totalHeightInInches =
+            Number(heightInInches) + Number(feetToInches)
+          const bmiImperial =
+            (weightFactor / Math.pow(totalHeightInInches, 2)) * 703
+          return displayResult(bmiImperial)
         }
       }
     }
 
-    const handleBMRCalculation = (weightFactor, heightFactor, ageFactor, genderFactor) => weightFactor + heightFactor - ageFactor + genderFactor
+    const handleBMRCalculation = (
+      weightFactor,
+      heightFactor,
+      ageFactor,
+      genderFactor
+    ) => {
+      if (!weightFactor || !heightFactor || !ageFactor || !genderFactor) {
+        return 0
+      } else {
+        return weightFactor + heightFactor - ageFactor + genderFactor
+      }
+    }
 
-    const calculateBMR = ({ age, gender, heightInCm, heightInFeet, heightInInches, weightInKg, weightInLbs, unitType } = this.props.personalInfo) => {
-      const weightFactorMetric = weight => Number(weight * 10)
-      const weightFactorImperial = weight => Number(weight * 4.536)
-      const heightFactorMetric = height => Number(height * 6.25)
-      const heightFactorImperial = height => Number(height * 15.88)
-      const ageFactor = age => Number(age * 5)
-      function genderFactor (gender) {
+    const calculateBMR = () => {
+      const weightFactorMetric = (weight) => Number(weight * 10)
+      const weightFactorImperial = (weight) => Number(weight * 4.536)
+      const heightFactorMetric = (height) => Number(height * 6.25)
+      const heightFactorImperial = (height) => Number(height * 15.88)
+      const ageFactor = (age) => Number(age * 5)
+      const genderFactor = (gender) => {
+        if (!gender) {
+          return 0
+        }
         if (gender === 'male') {
           return 5
         } else {
@@ -70,49 +83,39 @@ class DisplayResults extends React.Component {
       }
 
       if (unitType === 'metric') {
-        if (
-          !age || parseInt(age) === 0 ||
-          Boolean(gender) === false ||
-          Boolean(heightInCm) === false ||
-          Boolean(weightInKg) === false
-        ) {
-          return 0
-        } else {
-          return handleBMRCalculation(
-            weightFactorMetric(weightInKg),
-            heightFactorMetric(heightInCm),
-            ageFactor(age),
-            genderFactor(gender)
-          ).toFixed(2)
-        }
+        return handleBMRCalculation(
+          weightFactorMetric(weightInKg),
+          heightFactorMetric(heightInCm),
+          ageFactor(age),
+          genderFactor(gender)
+        ).toFixed(0)
+      } else if (unitType === 'imperial') {
+        const feetToInches = convert(heightInFeet).from('ft').to('in')
+        const totalHeightInInches =
+          Number(heightInInches) + Number(feetToInches)
+        return handleBMRCalculation(
+          weightFactorImperial(weightInLbs),
+          heightFactorImperial(totalHeightInInches),
+          ageFactor(age),
+          genderFactor(gender)
+        ).toFixed(0)
       }
+    }
 
-      if (unitType === 'imperial') {
-        if (
-          !age || parseInt(age) === 0 ||
-          Boolean(age) === false ||
-          Boolean(gender) === false ||
-          Boolean(weightInLbs) === false
-        ) {
-          return 0
-        } else if (Boolean(heightInFeet) === false && Boolean(heightInInches) === false) {
-          return 0
-        } else {
-          const feetToInches = convert(heightInFeet).from('ft').to('in')
-          const totalHeightInInches = Number(heightInInches) + Number(feetToInches)
-          return handleBMRCalculation(
-            weightFactorImperial(weightInLbs),
-            heightFactorImperial(totalHeightInInches),
-            ageFactor(age),
-            genderFactor(gender)
-          ).toFixed(2)
-        }
-      }
+    const calculateTDEE = () => {
+      return Math.floor(calculateBMR() * activityLevel)
     }
 
     if (unitType === 'metric') {
       calculateBMI(heightInCm, weightInKg)
-      calculateBMR(heightInCm, heightInFeet, heightInInches, weightInKg, weightInLbs, unitType)
+      calculateBMR(
+        heightInCm,
+        heightInFeet,
+        heightInInches,
+        weightInKg,
+        weightInLbs,
+        unitType
+      )
       calculateTDEE(activityLevel)
     } else {
       if (unitType === 'imperial') {
@@ -127,7 +130,7 @@ class DisplayResults extends React.Component {
         return ''
       }
       if (!isFinite(bmiFn) || isNaN(bmiFn)) {
-        return <span style={{ fontSize: '1.5rem' }}>Sp∞ky</span>
+        return <span style={{ fontSize: '1.5rem' }}>Invalid values</span>
       }
       if (bmiFn < 18.5) {
         return 'Underweight! A healthy BMI is between 18.5 and 25.'
@@ -144,17 +147,13 @@ class DisplayResults extends React.Component {
     }
 
     return (
-
-      <section data-testid="DisplayResults" className="tdee-results">
-
+      <section data-testid='DisplayResults' className='tdee-results'>
         <h1>Results</h1>
         <h2>BMI: {calculateBMI()}</h2>
         <span style={{ maxWidth: '150px' }}>{messageBMI(calculateBMI())}</span>
         <h2>BMR: {calculateBMR()} calories a day</h2>
         <h2>TDEE: {calculateTDEE()} calories a day</h2>
-
       </section>
-
     )
   }
 }
