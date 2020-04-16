c63833ad92144e37becd0b73e05e1f6d742103c7
nevtay <nevintay@gmail.com>
2020-04-16 23:38:16 +0800

    Fix bug where BMR would show despite 'age' field being empty or equal to '0'

--- src/containers/DisplayResults.js
+++ src/containers/DisplayResults.js
@@ -18,7 +18,7 @@ class DisplayResults extends React.Component {
 
     const calculateBMI = ({ heightInCm, heightInFeet, heightInInches, weightInKg, weightInLbs, unitType } = this.props.personalInfo) => {
       if (unitType === 'metric') {
-        if (Boolean(heightInCm) === false || Boolean(weightInKg) === false) {
+        if (heightInCm === '' || weightInKg === '') {
           return 0
         }
         const weightFactor = Number(weightInKg)
@@ -27,9 +27,19 @@ class DisplayResults extends React.Component {
         return result.toFixed(2)
       } else {
         if (unitType === 'imperial') {
-          if ((Boolean(weightInLbs) === false) || (Boolean(heightInFeet) === false && Boolean(heightInInches) === false)) {
+          if (!heightInInches && !heightInFeet) {
             return 0
           }
+          if (weightInLbs === '') {
+            weightInLbs = 0
+          }
+          if (heightInInches === '') {
+            heightInInches = 0
+          }
+          if (heightInFeet === '') {
+            heightInFeet = 0
+          }
+          console.log(weightInLbs, heightInFeet, heightInInches)
           const weightFactor = Number(weightInLbs)
           const feetToInches = convert(heightInFeet).from('ft').to('in')
           const totalHeightInInches = Number(heightInInches) + Number(feetToInches)
@@ -39,47 +49,49 @@ class DisplayResults extends React.Component {
       }
     }
 
-    const calculateBMR = ({ gender, heightInCm, heightInFeet, heightInInches, weightInKg, weightInLbs, unitType } = this.props.personalInfo) => {
+    const result = (weightFactor, heightFactor, ageFactor, genderFactor) => weightFactor + heightFactor - ageFactor + genderFactor
+
+    const calculateBMR = ({ age, gender, heightInCm, heightInFeet, heightInInches, weightInKg, weightInLbs, unitType } = this.props.personalInfo) => {
       const weightFactorMetric = weight => Number(weight * 10)
       const weightFactorImperial = weight => Number(weight * 4.536)
       const heightFactorMetric = height => Number(height * 6.25)
       const heightFactorImperial = height => Number(height * 15.88)
       const ageFactor = age => Number(age * 5)
-      const genderFactor = (gender) => {
+      function genderFactor (gender) {
         if (gender === 'male') {
           return 5
         } else {
           return -161
         }
       }
-      const result = (weightFactor, heightFactor, ageFactor, genderFactor) => weightFactor + heightFactor - ageFactor + genderFactor
-
       if (unitType === 'metric') {
         if (
-          Boolean(age) === false ||
+          !age || parseInt(age) === 0 ||
           Boolean(gender) === false ||
           Boolean(heightInCm) === false ||
           Boolean(weightInKg) === false
         ) {
           return 0
+        } else {
+          return result(
+            weightFactorMetric(weightInKg),
+            heightFactorMetric(heightInCm),
+            ageFactor(age),
+            genderFactor(gender)
+          ).toFixed(2)
         }
-        return result(
-          weightFactorMetric(weightInKg),
-          heightFactorMetric(heightInCm),
-          ageFactor(age),
-          genderFactor(gender)
-        ).toFixed(2)
-      } else {
-        if (unitType === 'imperial') {
-          if (
-            Boolean(age) === false ||
-            Boolean(gender) === false ||
-            Boolean(heightInFeet) === false ||
-            Boolean(heightInInches) === false ||
-            Boolean(weightInLbs) === false
-          ) {
-            return 0
-          }
+      }
+      if (unitType === 'imperial') {
+        if (
+          !age || parseInt(age) === 0 ||
+          Boolean(age) === false ||
+          Boolean(gender) === false ||
+          Boolean(weightInLbs) === false
+        ) {
+          return 0
+        } else if (Boolean(heightInFeet) === false && Boolean(heightInInches) === false) {
+          return 0
+        } else {
           const feetToInches = convert(heightInFeet).from('ft').to('in')
           const totalHeightInInches = Number(heightInInches) + Number(feetToInches)
           return result(
@@ -132,7 +144,7 @@ class DisplayResults extends React.Component {
 
         <h1>Results</h1>
         <h2>BMI: {calculateBMI()}</h2>
-        <span style={{ maxWidth: '150px' }}>{messageBMI(calculateBMI())}</span>
+        <span style={{ maxWidth: '150px' }}>{messageBMI(calculateBMI)}</span>
         <h2>BMR: {calculateBMR()} calories a day</h2>
         <h2>TDEE: {calculateTDEE()} calories a day</h2>
 
